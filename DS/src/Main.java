10e2ac16deed43cdd8f45638a2d2a057ca65128a
sophharris1998 <117087805+sophharris1998@users.noreply.github.com>
2023-02-06 13:14:42 +0000

    code along and chall1

--- DS/src/Main.java
+++ DS/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
+        String[] names = {"sophie", "Daniel", "Yuri", "Pedi"};
+
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+
+        //foreach
+        for (int num : nums) {
+            System.out.println(num);
+        }
+
+        //CHALL1
+        for (String name : names){
+            System.out.println("My name is " + name);
+        }
     }
 }
